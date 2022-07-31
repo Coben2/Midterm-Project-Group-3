06357799d54270d8ccd67ca50faa1e420ab14c2f
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-07-31 09:16:40 -0400

    Update PowerUp.cs - Refactor Level 1
    Refactor the code - removed extra spaces/ lines

--- Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/UI/HUD/PowerUp.cs
+++ Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/UI/HUD/PowerUp.cs
@@ -5,22 +5,16 @@ using TowerDefense.Level;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class PowerUp : MonoBehaviour
 {
 	public Button powerUpButton;
-
-	Currency m_Currency;
-
-	public int powerUpCost = 20; //currency required to powerUp
+    Currency m_Currency;
+    public int powerUpCost = 20; //currency required to powerUp
 
     [SerializeField] private int powerUpResetCost = 5; //value that remains fixed to restart the powerUp
-
     [SerializeField] private int powerUpReset = 100; //prevents user from being able to click again if score is >= powerUpCost
-
     [SerializeField] private int defaultPowerUpCost = 20; //if currency <= powerUpResetCost then currency required to powerUp resets to default
 
-   
     private void Start()
     {
 		powerUpButton.interactable = false;
@@ -30,10 +24,8 @@ public class PowerUp : MonoBehaviour
     void Update()
     {
 		m_Currency = LevelManager.instance.currency;
-     
         powerUpButton.onClick.AddListener(() => powerUpButton_onClick());
         UpdatePowerUpButton();
-
 	}
 
     void powerUpButton_onClick()
@@ -42,21 +34,18 @@ public class PowerUp : MonoBehaviour
         powerUpCost = powerUpReset;
     }
 
+    // Enable button
     void UpdatePowerUpButton()
     {
         { 
-            // Enable button
-                if (m_Currency.currentCurrency >= powerUpCost && !powerUpButton.interactable)
-                {
-                    powerUpButton.interactable = true;
-                }
-
-                else if (m_Currency.currentCurrency <= powerUpResetCost)
-                {
-                    powerUpCost = defaultPowerUpCost;
-                }
-               
+            if (m_Currency.currentCurrency >= powerUpCost && !powerUpButton.interactable)
+            {
+                powerUpButton.interactable = true;
+            }
+            else if (m_Currency.currentCurrency <= powerUpResetCost)
+            {
+                powerUpCost = defaultPowerUpCost;
+            } 
         }
-
     }
 }
